e2cee08c8cb7337a38e7ef895bf2dd94d0f321a1
duongtran2004 <S3979381@rmit.edu.vn>
2024-04-28 12:50:35 +0700

    Add PolicyHolderController, but not yet tested

--- src/main/java/org/example/insurancemanagementapplication/Controller/PolicyHolderController.java
+++ src/main/java/org/example/insurancemanagementapplication/Controller/PolicyHolderController.java
@@ -4,6 +4,22 @@ import Entity.Claim;
 import Entity.Dependant;
 import Entity.PolicyHolder;
 import jakarta.persistence.EntityManager;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.example.insurancemanagementapplication.DAO.PolicyHolderDAO;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * @author Luong Thanh Trung
@@ -11,7 +27,7 @@ import jakarta.persistence.EntityManager;
  * @created 27/04/2024 04:54
  * @project InsuranceManagementTeamProject
  */
-public class PolicyHolderController {
+public class PolicyHolderController implements Initializable {
     private final EntityManager entityManager;
     private final PolicyHolder policyHolder;
     private Dependant dependant;
@@ -44,5 +60,41 @@ public class PolicyHolderController {
 
     public EntityManager getEntityManager() {
         return entityManager;
+    }
+    @FXML
+    private Button logInButton;
+    @FXML
+    private Label errorContainer;
+    @FXML
+    private TextField userIdField;
+    @FXML
+    private TextField passwordField;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        logInButton.setOnAction(event -> {
+            String userId = userIdField.getText();
+            String password = passwordField.getText();
+            PolicyHolderDAO PolicyHolderDAO = new PolicyHolderDAO((Connection) entityManager);
+            try {
+                PolicyHolder authenticatedPolicyHolder = PolicyHolderDAO.authenticate(userId, password);
+                if (authenticatedPolicyHolder != null) {
+                    // Authentication successful, navigate to the System Admin dashboard
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("DashBoard_PolicyHolder.fxml"));
+                    Parent root = loader.load();
+                    Scene scene = new Scene(root);
+                    Stage stage = (Stage) logInButton.getScene().getWindow(); // Get the current stage
+                    stage.setScene(scene);
+                    stage.show();
+                } else {
+                    // Authentication failed, display error message
+                    errorContainer.setText("User not found");
+                }
+            } catch (SQLException e) {
+                // Handle SQLException
+                e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
